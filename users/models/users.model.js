2185b472a8b5b28202167e160bd85399e83a1a17
uday <udkumar@hotmail.com>
2019-08-09 23:48:15 +0530

    updated action and model

--- users/models/users.model.js
+++ users/models/users.model.js
@@ -9,3 +9,50 @@ const userSchema = new Schema({
 
 // attach the above schema to the user model.
 const userModel = mongoose.model('Users', userSchema);
+
+//add the createUser methodcd 
+exports.createUser = (userData) => {
+    const user = new User(userData);
+    return user.save();
+};
+
+
+// add the findById method 
+exports.findById = (id) => {
+    return User.findById(id).then((result) => {
+        result = result.toJSON();
+        delete result._id;
+        delete result.__v;
+        return result;
+    });
+};
+
+exports.patchUser = (id, userData) => {
+    return new Promise((resolve, reject) => {
+        User.findById(id, function (err, user) {
+            if (err) reject(err);
+            for (let i in userData) {
+                user[i] = userData[i];
+            }
+            user.save(function (err, updatedUser) {
+                if (err) return reject(err);
+                resolve(updatedUser);
+            });
+        });
+    })
+};
+
+exports.list = (perPage, page) => {
+    return new Promise((resolve, reject) => {
+        User.find()
+            .limit(perPage)
+            .skip(perPage * page)
+            .exec(function (err, users) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(users);
+                }
+            })
+    });
+};
